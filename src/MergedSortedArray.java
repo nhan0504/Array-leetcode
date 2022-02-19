176a5f996a9825b3d801bddd39348334596666f8
nhan0504 <ttn0504ltt@gmail.com>
2022-02-18 20:29:24 -0500

    Delete uneccessary cases

--- src/MergedSortedArray.java
+++ src/MergedSortedArray.java
@@ -5,38 +5,30 @@ public class MergedSortedArray {
         }
     }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        if (n == 0) {
-            return;
-        } else if (m == 0) {
-            for (int i = 0; i < n; i++) {
-                nums1[i] = nums2[i];
+        int i = 0;
+        int j = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                shift(i, nums1, m);
+                nums1[i] = nums2[j];
+                i++;
+                j++;
+                m++;
+            } else if (nums1[i] == nums2[j]) {
+                i++;
+                shift(i, nums1, m);
+                nums1[i] = nums2[j];
+                i++;
+                j++;
+                m++;
             }
-        } else {
-            int i = 0;
-            int j = 0;
-            while (i < m && j < n) {
-                if (nums1[i] < nums2[j]) {
-                    i++;
-                } else if (nums1[i] > nums2[j]) {
-                    shift(i, nums1, m);
-                    nums1[i] = nums2[j];
-                    i++;
-                    j++;
-                    m++;
-                } else if (nums1[i] == nums2[j]) {
-                    i++;
-                    shift(i, nums1, m);
-                    nums1[i] = nums2[j];
-                    i++;
-                    j++;
-                    m++;
-                }
-            }
-            if (j < n) {
-                for (int k = m; k < nums1.length; k++) {
-                    nums1[k] = nums2[j];
-                    j++;
-                }
+        }
+        if (j < n) {
+            for (int k = m; k < nums1.length; k++) {
+                nums1[k] = nums2[j];
+                j++;
             }
         }
     }
