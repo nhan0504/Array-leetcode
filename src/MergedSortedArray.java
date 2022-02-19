8a5e4504c30932c0c223827533d4a4b85f3b564a
nhan0504 <ttn0504ltt@gmail.com>
2022-02-18 21:08:51 -0500

    Clean code

--- src/MergedSortedArray.java
+++ src/MergedSortedArray.java
@@ -4,19 +4,9 @@ public class MergedSortedArray {
         int last2 = n - 1;
         int last = m + n - 1;
         while (last1 >= 0 && last2 >= 0) {
-            if (nums1[last1] > nums2[last2]) {
-                nums1[last] = nums1[last1];
-                last1--;
-            } else {
-                nums1[last] = nums2[last2];
-                last2--;
-            }
-            last--;
+            nums1[last--] = (nums1[last1] > nums2[last2]) ? nums1[last1--] : nums2[last2--];
         }
         while (last2 >= 0) {
-            nums1[last] = nums2[last2];
-            last2--;
-            last--;
+            nums1[last--] = nums2[last2--];
         }
     }
-}
